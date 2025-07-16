f9a58cec8ba8cf13fb51898bc246c6f2319ec495
sachin_sparky <sachin2322006@gmail.com>
2025-07-16 17:16:21 +0530

    some corrections

--- Backend/src/Lib/utils.js
+++ Backend/src/Lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: false,
-    secure: true",
+    sameSite: "None", 
+    secure: true,
   });
 
   return token;
