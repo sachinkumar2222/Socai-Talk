36a504e217070e02ebb4d10f87fd577c956337ba
sachin_sparky <sachin2322006@gmail.com>
2025-09-11 16:13:25 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://social-talk.onrender.com",
+    baseURL: "https://socai-talk.onrender.com",
     withCredentials: true,
 })
