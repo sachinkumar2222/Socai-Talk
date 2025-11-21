9f8458ae74ac0b403e8a9c5fae76a22821eaae0e
sachin_sparky <sachin2322006@gmail.com>
2025-11-21 22:57:16 +0530

    Refactor Server.js for code consistency and clarity

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -1,30 +1,32 @@
 import express from "express";
 import env from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
-import { connectDb } from "./Lib/db.js"
-import {app,server} from "./Lib/socket.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import { connectDb } from "./Lib/db.js";
+import { app, server } from "./Lib/socket.js";
 
 env.config();
 
+// Allow access from anywhere
 app.use(cors({
-    origin: "https://social-talk.vercel.app",
-    credentials: true
+  origin: true,
+  credentials: true
 }));
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes)
-app.use("/api/message",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/message", messageRoutes);
 
-const PORT= process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
-server.listen(PORT,()=>{
-    console.log(`your server run on http://localhost:${PORT}`);
-    connectDb();
-})
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  connectDb();
+});
