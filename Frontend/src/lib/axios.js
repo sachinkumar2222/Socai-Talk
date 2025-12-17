c08eb48ad796ef48978a855bb8cf670fd1079ae5
sachinkumar2222 <sachin2322006@gmail.com>
2025-12-17 20:36:48 +0530

    build: revert axios baseURL to deployed environment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: "https://socai-talk.onrender.com/api", 
-    baseURL: "http://localhost:5000",
+    baseURL: "https://socai-talk.onrender.com/api",
+    // baseURL: "http://localhost:5000",
     withCredentials: true,
 })
