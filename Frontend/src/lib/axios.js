ea336562620220438aeb00c1784e54b471a13597
sachinkumar2222 <sachin2322006@gmail.com>
2025-12-17 20:48:24 +0530

    fix: remove duplicate /api from axios baseURL

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://socai-talk.onrender.com/api",
+    baseURL: "https://socai-talk.onrender.com",
     // baseURL: "http://localhost:5000",
     withCredentials: true,
 })
